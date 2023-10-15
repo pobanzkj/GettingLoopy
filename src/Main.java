e1e1815756413e750d9b6fa1160167bc6d790aec
pobanzkj <144830435+pobanzkj@users.noreply.github.com>
2023-10-15 13:21:49 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,58 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        for(int num = 0; num <= 30; num ++)
+        {
+            System.out.println(num);
+        }
+
+        for(int cnt = 30; cnt >= 0; cnt --)
+        {
+            System.out.println(cnt);
+        }
+
+        for(int num = 0; num <= 18; num = num + 3)
+        {
+            System.out.println(num);
+        }
+
+        for(int num = 10; num >= 0; num = num - 2)
+        {
+            System.out.println(num);
+        }
+
+        for(int r = 1; r <= 5; r++)
+        {
+            for(int c = 1; c <= 5; c++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for(int r = 1; r <= 5; r++)
+        {
+            for(int c = 1; c <= r; c++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int r = 5; r >= 1; r--)
+        {
+            for(int c = 1; c <= r; c++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
+
